1752b8726f7e4a8e59ee2a138dac7b6cd068b065
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-03-02 11:44:59 +0100

    fix: map the goals

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,14 @@
 import { View } from 'react-native';
 import { GoalSection } from "../components/GoalSection/GoalSection.js";
+import { useGoals } from '../hooks/useGoals.js';
 
 export const Home = () => {
+    const { goals } = useGoals();
     return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#1e2027' }}>
-        <GoalSection title="Apprendre React Native" progress={75} creationDate="2024-01-15" description="Développer une application mobile avec React Native." />
+        {goals.map(goal => (
+            <GoalSection key={goal.id} title={goal.title} progress={goal.progress} creationDate={goal.creationDate} description={goal.description} />
+        ))}
     </View>
     );
 };
