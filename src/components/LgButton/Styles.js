8078dc7c32aacb7d1b3f89fda60ca909e9574d4f
Ilias HANFAOUI <ilias.hanfaoui@decathlon.com>
2026-03-02 11:56:30 +0100

    feat: add a goal button

---
+++ src/components/LgButton/Styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#337435',
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        borderRadius: 8,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
